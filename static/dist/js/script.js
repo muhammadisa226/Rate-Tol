529fa56cb6a3bf09ef3c96dc506789572464e02a
Muhammad Isa Nuruddin <75399939+muhammadisa226@users.noreply.github.com>
2022-11-05 02:39:26 +0700

    add ui chatbot css & js done remove dropdown menu

--- static/dist/js/script.js
+++ static/dist/js/script.js
@@ -5,11 +5,3 @@ const chatboxMessage = document.querySelector('.chatbox-message-wrapper')
 chatboxToggle.addEventListener('click', function () {
     chatboxMessage.classList.toggle('show')
 })
-
-//dropdown toggle
-const dropdownToggle = document.querySelector('chatbox-message-dropdown-toggle')
-const dropdownMenu = document.querySelector('chatbox-message-dropdown-menu')
-
-dropdownToggle.addEventListener('click', function(){
-    dropdownMenu.classList.toggle('show')
-})
